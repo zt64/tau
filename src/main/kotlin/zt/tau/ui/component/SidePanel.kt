f2136f70dc8a67e2481a24bc0d2178c0e60196ae
zt64 <nwowens32@gmail.com>
2022-08-30 22:10:54 -0400

    Remove placeholder bookmarks

--- src/main/kotlin/zt/tau/ui/component/SidePanel.kt
+++ src/main/kotlin/zt/tau/ui/component/SidePanel.kt
@@ -4,14 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import zt.tau.model.Bookmark
-import zt.tau.ui.component.sidepanel.Bookmark
-import kotlin.io.path.Path
 
 @Composable
 fun SidePanel() {
@@ -23,14 +19,7 @@ fun SidePanel() {
             modifier = Modifier.width(120.dp), // eventually fetch from settings
             verticalArrangement = Arrangement.spacedBy(2.dp)
         ) {
-            itemsIndexed(Array(20) { "" }) { index, t ->
-                Bookmark(
-                    data = Bookmark(Path("")),
-                    onClick = {
-                        // navigate to path
-                    }
-                )
-            }
+
         }
     }
 }
