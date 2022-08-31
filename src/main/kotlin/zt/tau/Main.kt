58022773dbb0a6554fa0f9a1582818ab6db66bc7
zt64 <nwowens32@gmail.com>
2022-08-30 22:11:05 -0400

    Add path argument

--- src/main/kotlin/zt/tau/Main.kt
+++ src/main/kotlin/zt/tau/Main.kt
@@ -4,6 +4,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
+import com.github.ajalt.clikt.parameters.types.path
 import zt.tau.ui.theme.TauTheme
 import zt.tau.ui.window.BrowserWindow
 import java.awt.Dimension
@@ -11,6 +14,8 @@ import java.awt.Dimension
 fun main(args: Array<String>) = Tau().main(args)
 
 private class Tau : CliktCommand() {
+    val path by argument().path(mustExist = true, canBeFile = false).optional()
+
     override fun run() = application {
         TauTheme {
             Window(
