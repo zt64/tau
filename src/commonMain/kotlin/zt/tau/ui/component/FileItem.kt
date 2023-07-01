932bac3332ea1cc93111d28c0ad4498f70a26712
letters <46039037+keli5@users.noreply.github.com>
2023-07-01 02:14:47 -0500

    Add a slight drop shadow to item names

--- src/commonMain/kotlin/zt/tau/ui/component/FileItem.kt
+++ src/commonMain/kotlin/zt/tau/ui/component/FileItem.kt
@@ -2,10 +2,7 @@ package zt.tau.ui.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Folder
@@ -18,6 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -120,10 +121,15 @@ fun FileItem(
                             )
                         }
                     }
-
                     Text(
                         text = path.name,
-                        style = MaterialTheme.typography.labelMedium,
+                        style = MaterialTheme.typography.labelMedium.copy(
+                            shadow = Shadow(
+                                color = MaterialTheme.colorScheme.outline,
+                                offset = Offset.Zero,
+                                blurRadius = 0.75f
+                            )
+                        ),
                         textAlign = TextAlign.Center
                     )
                 }
