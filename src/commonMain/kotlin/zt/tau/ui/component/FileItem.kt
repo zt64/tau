3d4db116eb7fceb59c96b6575aa052bd9f050377
zt64 <nwowens32@gmail.com>
2023-07-03 01:29:16 -0400

    feat(FileItem): improve file handling and UI updates
    Implement delete functionality

--- src/commonMain/kotlin/zt/tau/ui/component/FileItem.kt
+++ src/commonMain/kotlin/zt/tau/ui/component/FileItem.kt
@@ -2,35 +2,37 @@ package zt.tau.ui.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.apache.tika.Tika
 import zt.tau.ui.window.PropertiesWindow
-import zt.tau.ui.window.selectedFile
 import zt.tau.util.contains
 import zt.tau.util.setContents
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
 import java.io.File
-import java.net.URLConnection
 import java.nio.file.Path
-import javax.lang.model.type.NullType
-import kotlin.io.path.isReadable
-import kotlin.io.path.isRegularFile
-import kotlin.io.path.isSymbolicLink
-import kotlin.io.path.name
+import kotlin.io.path.*
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -39,15 +41,10 @@ fun FileItem(
     onClick: () -> Unit,
     onDoubleClick: () -> Unit,
     modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
     var showProperties by remember { mutableStateOf(false) }
-    val tika = Tika()
-    val (dataType, dataFormat) = try {
-        tika.detect(path.toFile().inputStream(), path.fileName.toString()).split("/")
-    } catch (_: Exception) {
-        listOf(null, null)
-    }
-
+    val clipboardManager = LocalClipboardManager.current
 
     if (showProperties) {
         PropertiesWindow(
@@ -56,116 +53,127 @@ fun FileItem(
         )
     }
 
-    Column(
-        modifier = Modifier
-            .combinedClickable(
-                onClick = onClick,
-                onDoubleClick = onDoubleClick
+    ContextMenuArea(
+        items = {
+            listOf(
+                ContextMenuItem("Copy") {
+                    clipboardManager.setContents(FileTransferable(listOf(path.toFile())), null)
+                },
+                ContextMenuItem("Cut") {
+
+                },
+                ContextMenuItem("Delete") {
+                    path.deleteIfExists()
+                },
+                ContextMenuItem("Properties") {
+                    showProperties = true
+                }
             )
-            .then(modifier),
-        horizontalAlignment = Alignment.CenterHorizontally
+        }
     ) {
-        val clipboardManager = LocalClipboardManager.current
-
-        ContextMenuArea(
-            items = {
-                listOf(
-                    ContextMenuItem("Copy") {
-                        clipboardManager.setContents(FileTransferable(listOf(path.toFile())), null)
-                    },
-                    ContextMenuItem("Cut") {
-
-                    },
-                    ContextMenuItem("Delete") {
-
-                    },
-                    ContextMenuItem("Properties") {
-                        showProperties = true
-                    }
-                )
+        TooltipArea(
+            tooltip = {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    tonalElevation = 6.dp,
+                    shadowElevation = 4.dp
+                ) {
+                    Text(
+                        modifier = Modifier.padding(10.dp),
+                        text = path.name
+                    )
+                }
             }
         ) {
-            TooltipArea(
-                tooltip = {
-                    Surface(
-                        shape = MaterialTheme.shapes.medium,
-                        tonalElevation = 6.dp,
-                        shadowElevation = 4.dp
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(10.dp),
-                            text = path.name
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .combinedClickable(
+                        interactionSource = interactionSource,
+                        indication = LocalIndication.current,
+                        onClick = onClick,
+                        onDoubleClick = onDoubleClick
+                    )
+                    .then(modifier),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    val coroutineScope = rememberCoroutineScope { Dispatchers.IO }
+                    var fileIcon by remember {
+                        mutableStateOf(
+                            if (path.isDirectory()) Icons.Default.Folder else Icons.Default.Description
                         )
                     }
-                }
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Box {
-                        Icon(
-                            modifier = Modifier.size(48.dp),
-                            imageVector = when {
 
-                                path.isRegularFile() -> when (dataType) {
-                                    "image"         -> Icons.Default.Image
-                                    "video"         -> Icons.Default.Movie
-                                    "audio"         -> Icons.Default.MusicNote
-                                    "text"          -> Icons.Default.Article
-                                    "font"          -> Icons.Default.TextFields
-                                    "application"   -> when {
-                                        dataFormat!!.contains(
-                                            listOf("zip", "7z", "rar", "tar")
-                                        ) -> Icons.Default.FolderZip
+                    LaunchedEffect(Unit) {
+                        coroutineScope.launch {
+                            if (path.isDirectory()) return@launch
 
-                                        dataFormat == "java-archive" -> Icons.Default.Coffee
-                                        dataFormat == "ogg"          -> Icons.Default.MusicVideo
+                            val (dataType, dataFormat) = runCatching {
+                                Tika()
+                                    .detect(path.inputStream(), path.fileName.toString())
+                                    .split("/")
+                            }.getOrNull() ?: return@launch
 
-                                        else -> Icons.Default.Description
-                                    }
+                            fileIcon = when (dataType) {
+                                "image" -> Icons.Default.Image
+                                "video" -> Icons.Default.VideoFile
+                                "audio" -> Icons.Default.AudioFile
+                                "text" -> Icons.Default.Article
+                                "font" -> Icons.Default.TextFields
+                                "application" -> when {
+                                    dataFormat.contains("zip", "7z", "rar", "tar") -> Icons.Default.Archive
+                                    dataFormat == "java-archive" -> Icons.Default.Coffee
+                                    dataFormat == "ogg" -> Icons.Default.MusicVideo
 
                                     else -> Icons.Default.Description
                                 }
 
-                                else -> Icons.Default.Folder
-                            },
-                            tint = MaterialTheme.colorScheme.primary,
-                            contentDescription = null
-                        )
-
-                        val icon = when {
-                            path.isSymbolicLink() -> Icons.Default.Link
-                            !path.isReadable() -> Icons.Default.Lock
-                            else -> null
-                        }
-
-                        if (icon != null) {
-                            Icon(
-                                modifier = Modifier
-                                    .size(28.dp)
-                                    .align(Alignment.BottomEnd),
-                                imageVector = icon,
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                                contentDescription = null
-                            )
+                                else -> Icons.Default.Description
+                            }
                         }
                     }
-                    Text(
-                        text = if (path.name.length > 15 && (selectedFile != path) ) {
-                            path.name.take(15) + "..."
-                        } else {
-                            path.name
-                        },
-                        style = MaterialTheme.typography.labelMedium.copy(
-                            shadow = Shadow(
-                                color = MaterialTheme.colorScheme.outline,
-                                offset = Offset.Zero,
-                                blurRadius = 0.75f
-                            )
-                        ),
-                        textAlign = TextAlign.Center
+
+                    Icon(
+                        modifier = Modifier.fillMaxSize(),
+                        imageVector = fileIcon,
+                        tint = MaterialTheme.colorScheme.primary,
+                        contentDescription = null
                     )
+
+                    val icon = when {
+                        path.isSymbolicLink() -> Icons.Default.Link
+                        !path.isReadable() -> Icons.Default.Lock
+                        else -> null
+                    }
+
+                    if (icon != null) {
+                        Icon(
+                            modifier = Modifier
+                                .fillMaxSize(0.3f)
+                                .align(Alignment.BottomEnd),
+                            imageVector = icon,
+                            tint = MaterialTheme.colorScheme.tertiary,
+                            contentDescription = null
+                        )
+                    }
                 }
+
+                Text(
+                    text = path.name,
+                    textAlign = TextAlign.Center,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3,
+                    style = MaterialTheme.typography.labelMedium.copy(
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.outline,
+                            offset = Offset.Zero,
+                            blurRadius = 0.75f
+                        )
+                    )
+                )
             }
         }
     }
