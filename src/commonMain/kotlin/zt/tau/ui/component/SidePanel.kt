8fb0fa302d5d6571cc50449fd8f10c1841026bb8
zt64 <nwowens32@gmail.com>
2023-07-04 00:49:03 -0400

    feat: update side panel

--- src/commonMain/kotlin/zt/tau/ui/component/SidePanel.kt
+++ src/commonMain/kotlin/zt/tau/ui/component/SidePanel.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Storage
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import zt.tau.ui.component.sidepanel.Bookmark
@@ -18,22 +20,25 @@ import java.io.File
 fun SidePanel() {
     Surface(
         modifier = Modifier.fillMaxHeight(),
-        tonalElevation = 2.dp
+        tonalElevation = 5.dp
     ) {
+        val roots = remember { File.listRoots() }
+
         LazyColumn(
-            modifier = Modifier.width(120.dp), // eventually fetch from settings
+            modifier = Modifier.width(160.dp), // eventually fetch from settings
             verticalArrangement = Arrangement.spacedBy(2.dp)
         ) {
-            File.listRoots().forEach {
-                item {
-                    Bookmark(
-                        zt.tau.model.Bookmark(it.toPath(), it.absolutePath),
-                        icon = Icons.Default.Storage,
-                        onClick = {
-                            currentLocation = it.toPath()
-                        }
-                    )
-                }
+            items(
+                items = roots,
+                key = { it.name }
+            ) {
+                Bookmark(
+                    zt.tau.model.Bookmark(it.toPath(), it.absolutePath),
+                    icon = Icons.Default.Storage,
+                    onClick = {
+                        currentLocation = it.toPath()
+                    }
+                )
             }
         }
     }
