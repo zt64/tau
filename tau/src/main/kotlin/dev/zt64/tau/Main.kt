b13579242df1e635eb9325c74996138912fc25a7
keli5 <46039037+keli5@users.noreply.github.com>
2023-12-08 20:58:04 -0600

    remove unused import

--- tau/src/main/kotlin/dev/zt64/tau/Main.kt
+++ tau/src/main/kotlin/dev/zt64/tau/Main.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import com.github.ajalt.clikt.core.CliktCommand
