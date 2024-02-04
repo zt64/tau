0ca7a45d13c7790380279d913cbaa55a3f186f37
zt64 <31907977+zt64@users.noreply.github.com>
2024-02-04 01:41:48 -0500

    refactor MiniTextButton component

--- tau/src/main/kotlin/dev/zt64/tau/ui/component/MiniTextButton.kt
+++ tau/src/main/kotlin/dev/zt64/tau/ui/component/MiniTextButton.kt
@@ -2,17 +2,15 @@ package dev.zt64.tau.ui.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.role
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 
 val ButtonDefaults.MiniTextButtonContentPadding: PaddingValues
@@ -22,7 +20,6 @@ val ButtonDefaults.MiniTextButtonContentPadding: PaddingValues
     )
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 @Composable
 fun MiniTextButton(
     onClick: () -> Unit,
@@ -36,40 +33,23 @@ fun MiniTextButton(
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     content: @Composable RowScope.() -> Unit
 ) {
-    val containerColor = colors.containerColor(enabled).value
-    val contentColor = colors.contentColor(enabled).value
-    val shadowElevation = elevation?.shadowElevation(enabled, interactionSource)?.value ?: 0.dp
-    val tonalElevation = elevation?.tonalElevation(enabled, interactionSource)?.value ?: 0.dp
 
     CompositionLocalProvider(
         LocalMinimumInteractiveComponentEnforcement provides false
     ) {
-        Surface(
-            onClick = onClick,
-            modifier = modifier.semantics { role = Role.Button },
-            enabled = enabled,
-            shape = shape,
-            color = containerColor,
-            contentColor = contentColor,
-            tonalElevation = tonalElevation,
-            shadowElevation = shadowElevation,
-            border = border,
-            interactionSource = interactionSource
-        ) {
-            CompositionLocalProvider(LocalContentColor provides contentColor) {
-                ProvideTextStyle(value = MaterialTheme.typography.labelMedium) {
-                    Row(
-                        Modifier
-                            .defaultMinSize(
-                                minWidth = ButtonDefaults.MinWidth,
-                                minHeight = 28.dp
-                            ).padding(contentPadding),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically,
-                        content = content
-                    )
-                }
-            }
+        ProvideTextStyle(value = MaterialTheme.typography.labelMedium) {
+            Button(
+                onClick = onClick,
+                modifier = modifier.heightIn(28.dp),
+                enabled = enabled,
+                shape = shape,
+                colors = colors,
+                elevation = elevation,
+                border = border,
+                contentPadding = contentPadding,
+                interactionSource = interactionSource,
+                content = content
+            )
         }
     }
 }
