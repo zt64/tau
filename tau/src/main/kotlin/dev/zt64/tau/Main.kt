2ad16b17676ec28784e7c945898adaf00e97404c
zt64 <31907977+zt64@users.noreply.github.com>
2024-02-22 21:25:24 -0500

    refactor main entrypoint

--- tau/src/main/kotlin/dev/zt64/tau/Main.kt
+++ tau/src/main/kotlin/dev/zt64/tau/Main.kt
@@ -1,9 +1,11 @@
 package dev.zt64.tau
 
+import androidx.compose.ui.window.application
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.types.path
+import org.jetbrains.skiko.setSystemLookAndFeel
 
 fun main(args: Array<String>) = EntryPoint().main(args)
 
@@ -15,5 +17,13 @@ private class EntryPoint : CliktCommand() {
             canBeFile = false
         ).optional()
 
-    override fun run() = tau(path)
+    override fun run() {
+        setSystemLookAndFeel()
+
+        application {
+            Tau(
+                onCloseRequest = ::exitApplication
+            )
+        }
+    }
 }
--- tau/src/main/kotlin/dev/zt64/tau/Tau.kt
+++ tau/src/main/kotlin/dev/zt64/tau/Tau.kt
@@ -10,8 +10,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import com.godaddy.android.colorpicker.HsvColor
 import dev.zt64.tau.di.managerModule
 import dev.zt64.tau.di.screenModelModule
@@ -21,149 +19,142 @@ import dev.zt64.tau.ui.component.MiniTextButton
 import dev.zt64.tau.ui.theme.Theme
 import dev.zt64.tau.ui.window.BrowserWindow
 import dev.zt64.tau.ui.window.preferences.ParentWindow
-import org.jetbrains.skiko.setSystemLookAndFeel
 import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
 import java.awt.Dimension
-import java.nio.file.Path
 
-fun tau(path: Path?) {
-    setSystemLookAndFeel()
+@Composable
+fun Tau(
+    onCloseRequest: () -> Unit
+) {
+    KoinApplication(
+        application = {
+            modules(managerModule, screenModelModule)
+        }
+    ) {
+        val preferencesManager = koinInject<PreferencesManager>()
 
-    application {
-        KoinApplication(
-            application = {
-                modules(managerModule, screenModelModule)
-            }
+        val updatedHsvColor by remember(preferencesManager.color) {
+            mutableStateOf(HsvColor.from(Color(preferencesManager.color)))
+        }
+
+        Theme(
+            seedColor = updatedHsvColor.toColor(),
+            isDarkTheme = preferencesManager.theme == Theme.DARK
         ) {
-            val windowState = rememberWindowState()
-            val prefsWindowState = rememberWindowState()
-
-            val preferencesManager = koinInject<PreferencesManager>()
-
             var preferencesWindowVisible by remember { mutableStateOf(false) }
 
-            val updatedHsvColor by remember(preferencesManager.color) {
-                mutableStateOf(HsvColor.from(Color(preferencesManager.color)))
+            if (preferencesWindowVisible) {
+                Window(
+                    onCloseRequest = { preferencesWindowVisible = false },
+                    title = Res.string.settings,
+                    resizable = true,
+                    icon = painterResource("window-icon.svg")
+                ) {
+                    ParentWindow()
+                }
             }
 
-            Theme(
-                seedColor = updatedHsvColor.toColor(),
-                isDarkTheme = preferencesManager.theme == Theme.DARK
+            Window(
+                title = "tau",
+                icon = painterResource("window-icon.svg"),
+                onCloseRequest = onCloseRequest
             ) {
-                if (preferencesWindowVisible) {
-                    Window(
-                        onCloseRequest = { preferencesWindowVisible = false },
-                        state = prefsWindowState,
-                        title = Res.string.settings,
-                        resizable = true,
-                        icon = painterResource("window-icon.svg")
-                    ) {
-                        ParentWindow()
-                    }
+                LaunchedEffect(Unit) {
+                    window.minimumSize = Dimension(300, 400)
                 }
-                Window(
-                    title = "tau",
-                    icon = painterResource("window-icon.svg"),
-                    onCloseRequest = ::exitApplication,
-                    state = windowState
-                ) {
-                    LaunchedEffect(Unit) {
-                        window.minimumSize = Dimension(300, 400)
-                    }
 
-                    Column {
-                        Surface(
-                            tonalElevation = 0.dp
+                Column {
+                    Surface(
+                        tonalElevation = 0.dp
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 6.dp, vertical = 2.dp),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 6.dp, vertical = 2.dp),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Box {
-                                    var showFileDialog by remember { mutableStateOf(false) }
+                            Box {
+                                var showFileDialog by remember { mutableStateOf(false) }
 
-                                    MiniTextButton(
-                                        onClick = { showFileDialog = true }
-                                    ) {
-                                        Text(Res.string.file)
-                                    }
-
-                                    DropdownMenu(
-                                        expanded = showFileDialog,
-                                        onDismissRequest = { showFileDialog = false }
-                                    ) {
-                                        DropdownMenuItem(
-                                            text = {
-                                                Text(Res.string.copy)
-                                            },
-                                            onClick = {
-                                            }
-                                        )
-                                        DropdownMenuItem(
-                                            text = {
-                                                Text(Res.string.settings)
-                                            },
-                                            onClick = {
-                                                preferencesWindowVisible = true
-                                                showFileDialog = false
-                                            }
-                                        )
-                                    }
+                                MiniTextButton(
+                                    onClick = { showFileDialog = true }
+                                ) {
+                                    Text(Res.string.file)
                                 }
 
-                                Box {
-                                    var showFileDialog by remember { mutableStateOf(false) }
+                                DropdownMenu(
+                                    expanded = showFileDialog,
+                                    onDismissRequest = { showFileDialog = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(Res.string.copy)
+                                        },
+                                        onClick = {
+                                        }
+                                    )
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(Res.string.settings)
+                                        },
+                                        onClick = {
+                                            preferencesWindowVisible = true
+                                            showFileDialog = false
+                                        }
+                                    )
+                                }
+                            }
 
-                                    MiniTextButton(
-                                        onClick = { showFileDialog = true }
-                                    ) {
-                                        Text(Res.string.edit)
-                                    }
+                            Box {
+                                var showFileDialog by remember { mutableStateOf(false) }
 
-                                    DropdownMenu(
-                                        expanded = showFileDialog,
-                                        onDismissRequest = { showFileDialog = false }
-                                    ) {
-                                        DropdownMenuItem(
-                                            text = {
-                                                Text(Res.string.copy)
-                                            },
-                                            onClick = {
-                                            }
-                                        )
-                                    }
+                                MiniTextButton(
+                                    onClick = { showFileDialog = true }
+                                ) {
+                                    Text(Res.string.edit)
                                 }
 
-                                Box {
-                                    var showFileDialog by remember { mutableStateOf(false) }
+                                DropdownMenu(
+                                    expanded = showFileDialog,
+                                    onDismissRequest = { showFileDialog = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(Res.string.copy)
+                                        },
+                                        onClick = {
+                                        }
+                                    )
+                                }
+                            }
 
-                                    MiniTextButton(
-                                        onClick = { showFileDialog = true }
-                                    ) {
-                                        Text(Res.string.view)
-                                    }
+                            Box {
+                                var showFileDialog by remember { mutableStateOf(false) }
 
-                                    DropdownMenu(
-                                        expanded = showFileDialog,
-                                        onDismissRequest = { showFileDialog = false }
-                                    ) {
-                                        DropdownMenuItem(
-                                            text = {
-                                                Text(Res.string.copy)
-                                            },
-                                            onClick = {
-                                            }
-                                        )
-                                    }
+                                MiniTextButton(
+                                    onClick = { showFileDialog = true }
+                                ) {
+                                    Text(Res.string.view)
+                                }
+
+                                DropdownMenu(
+                                    expanded = showFileDialog,
+                                    onDismissRequest = { showFileDialog = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(Res.string.copy)
+                                        },
+                                        onClick = {
+                                        }
+                                    )
                                 }
                             }
                         }
-
-                        BrowserWindow()
                     }
+
+                    BrowserWindow()
                 }
             }
         }
