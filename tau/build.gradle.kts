73cf3ddda6e6cefcd70b7d86a798497c278436ac
zt64 <31907977+zt64@users.noreply.github.com>
2024-05-01 00:22:45 -0400

    update compose and stuff

--- tau/build.gradle.kts
+++ tau/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.libres)
 }
