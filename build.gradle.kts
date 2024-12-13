7e5ccc1bde4dc766080cef0df94558c2830b9891
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-13 17:06:23 -0500

    update jvm target to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ allprojects {
 subprojects {
     tasks.withType<KotlinJvmCompile>().configureEach {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
+            jvmTarget = JvmTarget.JVM_21
         }
     }
 
