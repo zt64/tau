67c300d616d3695aec51a068cfa4d92e34489021
zt64 <31907977+zt64@users.noreply.github.com>
2025-03-17 00:51:00 -0400

    reorder BrowseView layout hierarchy

--- tau/src/main/kotlin/dev/zt64/tau/ui/widget/browse/BrowseView.kt
+++ tau/src/main/kotlin/dev/zt64/tau/ui/widget/browse/BrowseView.kt
@@ -1,5 +1,7 @@
 package dev.zt64.tau.ui.widget.browse
 
+import androidx.compose.foundation.ContextMenuArea
+import androidx.compose.foundation.ContextMenuItem
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -20,62 +22,62 @@ fun BrowseView(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    // FIXME: For some reason weight modifier doesnt work with ContextMenuArea
-    // ContextMenuArea(
-    //     items = {
-    //         buildList {
-    //             add(ContextMenuItem("Create New", {}))
-    //         }
-    //     }
-    // ) {
-        var startOffset by remember { mutableStateOf(Offset.Unspecified) }
-        var endOffset by remember { mutableStateOf(Offset.Unspecified) }
-        val tertiary = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f)
+    var startOffset by remember { mutableStateOf(Offset.Unspecified) }
+    var endOffset by remember { mutableStateOf(Offset.Unspecified) }
+    val tertiary = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f)
 
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .onDrag(
-                    onDragStart = { offset ->
-                        startOffset = offset
-                        endOffset = offset
-                    },
-                    onDrag = { dragAmount ->
-                        endOffset += dragAmount
-                    },
-                    onDragEnd = {
-                        startOffset = Offset.Unspecified
-                        endOffset = Offset.Unspecified
-                    }
-                )
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                    onClick = onClick
-                )
-                .drawWithCache {
-                    onDrawWithContent {
-                        drawContent()
-                        if (startOffset.isSpecified && endOffset.isSpecified) {
-                            val rectTopLeft = Offset(
-                                x = minOf(startOffset.x, endOffset.x),
-                                y = minOf(startOffset.y, endOffset.y)
-                            )
-                            val rectSize = Size(
-                                width = (startOffset.x - endOffset.x).absoluteValue,
-                                height = (startOffset.y - endOffset.y).absoluteValue
-                            )
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .onDrag(
+                onDragStart = { offset ->
+                    startOffset = offset
+                    endOffset = offset
+                },
+                onDrag = { dragAmount ->
+                    endOffset += dragAmount
+                },
+                onDragEnd = {
+                    startOffset = Offset.Unspecified
+                    endOffset = Offset.Unspecified
+                }
+            )
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            )
+            .drawWithCache {
+                onDrawWithContent {
+                    drawContent()
+                    if (startOffset.isSpecified && endOffset.isSpecified) {
+                        val rectTopLeft = Offset(
+                            x = minOf(startOffset.x, endOffset.x),
+                            y = minOf(startOffset.y, endOffset.y)
+                        )
+                        val rectSize = Size(
+                            width = (startOffset.x - endOffset.x).absoluteValue,
+                            height = (startOffset.y - endOffset.y).absoluteValue
+                        )
 
-                            drawRect(
-                                color = tertiary,
-                                topLeft = rectTopLeft,
-                                size = rectSize
-                            )
-                        }
+                        drawRect(
+                            color = tertiary,
+                            topLeft = rectTopLeft,
+                            size = rectSize
+                        )
                     }
                 }
+            }
+    ) {
+        // FIXME: For some reason weight modifier doesn't work with ContextMenuArea
+        ContextMenuArea(
+            items = {
+                buildList {
+                    add(ContextMenuItem("Create New", {}))
+                }
+            }
         ) {
             content()
         }
-    // }
+    }
 }
