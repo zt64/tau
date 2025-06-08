b09332839a391f5c9aa622e3dc7c7aeb17124e06
zt64 <31907977+zt64@users.noreply.github.com>
2025-06-08 13:15:34 -0400

    remove unused operating system identification

--- core/src/main/kotlin/dev/zt64/tau/util/PlatformUtil.kt
+++ core/src/main/kotlin/dev/zt64/tau/util/PlatformUtil.kt
@@ -6,14 +6,6 @@ import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.ClipboardOwner
 import java.awt.datatransfer.Transferable
 
-enum class OperatingSystem {
-    WINDOWS,
-    LINUX,
-    MAC,
-    SOLARIS,
-    UNKNOWN
-}
-
 val ClipboardManager.systemClipboard: Clipboard by lazy {
     Toolkit.getDefaultToolkit().systemClipboard
 }
@@ -22,26 +14,3 @@ fun ClipboardManager.setContents(
     content: Transferable,
     owner: ClipboardOwner?
 ) = systemClipboard.setContents(content, owner)
-
-fun getOperatingSystem(): OperatingSystem {
-    val os = System.getProperty("os.name").lowercase()
-    return when {
-        os.contains("win") -> {
-            OperatingSystem.WINDOWS
-        }
-
-        os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
-            OperatingSystem.LINUX
-        }
-
-        os.contains("mac") -> {
-            OperatingSystem.MAC
-        }
-
-        os.contains("sunos") -> {
-            OperatingSystem.SOLARIS
-        }
-
-        else -> OperatingSystem.UNKNOWN
-    }
-}
